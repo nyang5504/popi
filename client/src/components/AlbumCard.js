8f6fc2f9a2ab0f084db4a3670fc86727b7791e9e
nyang5504 <ninayang3.14@gmail.com>
2023-05-17 09:40:35 -0400

    fix permission to dance

--- client/src/components/AlbumCard.js
+++ client/src/components/AlbumCard.js
@@ -5,7 +5,7 @@ const AlbumCard = (props) => {
     const { album } = props;
 
     return (
-        <Link to={"/albumdashboard/" + album[2].replace(/\s+/g, '-').toLowerCase()}>
+        <Link to={"/albumdashboard/" + album[2].replace(/[\/\s]+/g, '-').toLowerCase()}>
             <div className="album-card">
                 <img src={album[0]} alt={album[1] + " " + album[2]} />
                 <div className="artist-name">{album[1]}</div>
