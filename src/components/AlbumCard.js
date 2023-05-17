2896f1401786f5a3382465cd094aa7c3aa701cd0
vanessanyc <elm.vanessa.louis@gmail.com>
2023-05-16 21:22:18 -0400

    fixed weird error

--- src/components/AlbumCard.js
+++ src/components/AlbumCard.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
 const AlbumCard = (props) => {
-    
-    return (
-      <div className="album-card">HELLO</div>
-    );
+  return (
+    <div className="album-card">
+      <img src={props.album.img} width="275" height="275" alt="Album Cover" />
+      <div className="album-name">
+        <p>{props.album.title}</p>
+      </div>
+      <div className="artist-name">
+        <p>{props.album.artist}</p>
+      </div>
+    </div>
+  );
 };
 
 export default AlbumCard;
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,12 +1,53 @@
 import AlbumCard from "../components/AlbumCard"
 
 const Homepage = (props) => {
-    return(
+    const albums = [
+        {
+          coverImageUrl: 'https://upload.wikimedia.org/wikipedia/en/5/51/Igor_-_Tyler%2C_the_Creator.jpg',
+          name: 'Album 1',
+          artist: 'Artist 1',
+        },
+        {
+          coverImageUrl: 'https://upload.wikimedia.org/wikipedia/en/5/51/Igor_-_Tyler%2C_the_Creator.jpg',
+          name: 'Album 2',
+          artist: 'Artist 2',
+        },
+        {
+            coverImageUrl: 'https://upload.wikimedia.org/wikipedia/en/5/51/Igor_-_Tyler%2C_the_Creator.jpg',
+            name: 'Album 2',
+            artist: 'Artist 2',
+        },
+        {
+            coverImageUrl: 'https://upload.wikimedia.org/wikipedia/en/5/51/Igor_-_Tyler%2C_the_Creator.jpg',
+            name: 'Album 1',
+            artist: 'Artist 1',
+        },
+        {
+            coverImageUrl: 'https://upload.wikimedia.org/wikipedia/en/5/51/Igor_-_Tyler%2C_the_Creator.jpg',
+            name: 'Album 2',
+            artist: 'Artist 2',
+        },
+        {
+            coverImageUrl: 'https://upload.wikimedia.org/wikipedia/en/5/51/Igor_-_Tyler%2C_the_Creator.jpg',
+            name: 'Album 2',
+            artist: 'Artist 2',
+        }
+      ];
+    
+      return (
         <div>
-            <AlbumCard/>
             <h1>HOMEPAGE</h1>
+            <div className="home-page">
+                <div className="album-grid2">
+                    <div className="album-grid">
+                    {albums.map(e => (
+                        <AlbumCard img={e.coverImageUrl} album={e.name} artistName={e.artist} />
+                    ))}
+                    </div>
+                </div>
+            </div>
         </div>
-    )
+      );
 };
 
 export default Homepage;
