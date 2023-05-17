7306b8b51aaca6b2467d51f7e4aa8630e2c1fb9e
vanessanyc <elm.vanessa.louis@gmail.com>
2023-05-17 09:22:50 -0400

    albums go to proper dashboard now

---
+++ client/src/components/ReviewCard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './ReviewCard.css';
+
+const ReviewCard = ({ review }) => {
+  return (
+    <div className="review-card">
+      <div className="review-user">
+        <img src={review.avatarUrl} alt="User avatar" />
+        <p>{review.username}</p>
+      </div>
+      <div className="review-text">
+        <p>{review.comment}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ReviewCard;
--- client/src/pages/AlbumDashboard.js
+++ client/src/pages/AlbumDashboard.js
@@ -1,14 +1,60 @@
+import React, { useContext } from 'react';
 import { useParams } from 'react-router-dom';
+import { AlbumContext } from '../components/AlbumContext';
+import AlbumCard from '../components/AlbumCard';
+import ReviewCard from '../components/ReviewCard';
 
-const AlbumDashboard = (props) => {
-    const { albumName } = useParams();
+const AlbumDashboard = () => {
+  const { albumName } = useParams();
+  const allinfo = useContext(AlbumContext);
 
-    return(
-        <div>
-            <h1>ALBUM DASHBOARD</h1>
-            <h2>Album: {albumName.replace(/-/g, ' ')}</h2>
-        </div>
+  // Find the album that matches the albumName URL parameter
+  const album = allinfo.find(album => album[2].replace(/\s+/g, '-').toLowerCase() === albumName);
+
+  // Static reviews data
+  const reviews = [
+    {
+      avatarUrl: '',
+      username: 'anon',
+      comment: 'Slaaaaaay!',
+    },
+    {
+      avatarUrl: '',
+      username: 'anon',
+      comment: 'SO GOOD!',
+    },
+    {
+      avatarUrl: '',
+      username: 'anon',
+      comment: 'obsessed',
+    },
+  ];
+
+  // If no matching album is found, display an error message
+  if (!album) {
+    return (
+      <div>
+        <h1>Album not found</h1>
+      </div>
     )
+  }
+
+  return (
+    <div className="album-dashboard">
+      <div className="left-side">
+        <AlbumCard album={album} />
+      </div>
+      <div className="right-side">
+        <div className="reviews-container">
+          {reviews.map((review, index) => (
+            <ReviewCard key={index} review={review} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AlbumDashboard;
+
+
