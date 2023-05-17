a99fadeae379331642a52171934eb4fd7452c80b
vanessanyc <elm.vanessa.louis@gmail.com>
2023-05-16 21:13:36 -0400

    making dashboard show up

--- src/pages/AlbumDashboard.js
+++ src/pages/AlbumDashboard.js
@@ -1,12 +1,47 @@
-import AlbumCard from "../components/AlbumCard";
+import React from 'react';
+//import AlbumInfo from './AlbumInfo';
+import ReviewCard from './ReviewCard';
+import ReviewForm from './ReviewForm';
+import AlbumCard from '../components/AlbumCard';
+import './AlbumDashboard.css';
 
 const AlbumDashboard = (props) => {
-    return(
-        <div>
-            {/* <AlbumCard/> */}
-            <h1>ALBUMDASHBOARD</h1>
+    const album = { title: 'My Album', artist: 'Artist Name' };
+
+    // Static reviews data
+  const reviews = [
+    {
+      avatarUrl: '',
+      username: 'anon',
+      comment: 'Slaaaaaay!',
+    },
+    {
+      avatarUrl: '',
+      username: 'anon',
+      comment: 'SO GOOD!',
+    },
+    {
+      avatarUrl: '',
+      username: 'anon',
+      comment: 'obsessed',
+    },
+  ];
+
+  return (
+        <div className="album-dashboard">
+        <div className="left-side">
+        <AlbumCard album={album} />
         </div>
-    )
+        <div className="right-side">
+        <div className="reviews-container">
+            {reviews.map((review, index) => (
+            <ReviewCard key={index} review={review} />
+            ))}
+        </div>
+        <ReviewForm />
+        </div>
+    </div>
+    );
 };
 
 export default AlbumDashboard;
